a861bcfbc0498270a68c71759d6ddb4d2cdfdb75
Vincent Tshipamba <tshipambalubobo80@gmail.com>
2024-10-15 15:28:56 +0100

    GEstion des exceptions et des redirections vers les pages d'erreurs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,14 +1,18 @@
 <?php
 
-use App\Http\Middleware\CheckCandidatSubmission;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
+use Illuminate\Database\QueryException;
+use App\Http\Middleware\CheckCandidatSubmission;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -17,5 +21,34 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (Exception $exception, Request $request) {
+
+            if ($exception instanceof NotFoundHttpException) {
+                return response()->view("errors.404", [], 404);
+            }
+
+            if ($exception instanceof HttpException && $exception->getStatusCode() == 400) {
+                //return response()->view("admin.errors.400", [], 400);
+            }
+
+            if ($exception instanceof HttpException && $exception->getStatusCode() == 403) {
+                //return response()->view("admin.errors.403", [], 403);
+            }
+
+            if ($exception instanceof HttpException && $exception->getStatusCode() == 500) {
+                return response()->view("errors.500", [], 500);
+            }
+
+            if ($exception instanceof QueryException) {
+                return response()->view(
+                    'errors.500',
+                    ['message' => "Il y a un problème de connexion à la base de données."],
+                    500
+                );
+            }
+
+            if ($exception instanceof HttpException && $exception->getStatusCode() == 503) {
+                return response()->view("errors.503", [], 503);
+            }
+        });
     })->create();
