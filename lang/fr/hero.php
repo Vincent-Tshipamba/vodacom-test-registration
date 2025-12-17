c838263ea852c40482aabff9736b756a842b9816
Vincent-Tshipamba <tshipambalubobo80@gmail.com>
2025-12-17 11:52:42 +0100

    Ajout d'un paragraphe manquant a la secion principale du site

--- lang/fr/hero.php
+++ lang/fr/hero.php
@@ -2,7 +2,7 @@
 
 return [
     'title' => 'Boostez votre avenir avec la bourse Vodacom',
-    'description' => 'La Fondation Vodacom soutient les nouveaux diplômés ambitieux...',
+    'description' => 'La Fondation Vodacom soutient les nouveaux diplômés ambitieux en leur offrant une bourse pour propulser leurs projets et leurs carrières.',
     'cta' => 'Postulez maintenant',
     'learn_more' => 'En savoir plus',
 ];
