1e998758d48a5bf7b949ddf2f347549817a6d40b
Vincent-Tshipamba <tshipambalubobo80@gmail.com>
2026-01-19 12:43:06 +0100

    Ajout de cles de langue pour l'affichage en francais des details d'un candidat

---
+++ lang/fr/admin.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+    'specific_condition' => 'Condition spécifique',
+    'first_name' => 'Prénom',
+    'last_name' => 'Nom de famille',
+    'date_of_birth' => 'Date de naissance',
+    'gender' => 'Genre',
+    'location' => 'Adresse complète',
+    'phone_number' => 'Numéro de téléphone',
+    'email' => 'Email',
+    'years_old' => 'ans',
+
+    // genders
+    'genders' => [
+        'male' => 'Masculin',
+        'female' => 'Féminin',
+        'other' => 'Autre',
+    ],
+
+    // Vulnerability types
+    'vulnerability_types' => [
+        'disabled' => 'Personne handicapée',
+        'albinos' => 'Albinos',
+        'pygmee' => 'Pygmée',
+        'refugee' => 'Réfugié',
+        'orphan' => 'Orphelin',
+        'none' => 'Aucun',
+    ],
+
+    'cities_info' => 'Informations sur les villes',
+    'diploma_city' => 'Ville d\'obtention du diplôme',
+    'current_city' => 'Ville actuelle',
+
+    'school_infos' => 'Informations scolaires',
+    'school_name' => 'Nom de l\'école',
+    'national_exam_code' => 'Code national d\'examen',
+    'percentage' => 'Pourcentage',
+    'option_studied' => 'Option étudiée',
+
+    'documents_infos' => 'Informations sur les documents',
+    'id' => 'Pièce d\'identité',
+    'diploma' => 'Diplôme ou attestation de réussite',
+    'photo' => 'Photo',
+    'reco_letter' => 'Lettre de recommandation',
+
+    'personal_ambitions' => 'Ambitions personnelles',
+    'intended_field' => 'Domaine d\'études envisagé',
+    'intended_field_motivation' => 'Motivation pour le domaine d\'études envisagé',
+    'career_goals' => 'Objectifs de carrière',
+    'additional_infos' => 'Informations supplémentaires',
+];
