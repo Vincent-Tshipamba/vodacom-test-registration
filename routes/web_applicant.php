95420679bf0a23c2c63571d78f5414aa437076bb
Vincent-Tshipamba <tshipambalubobo80@gmail.com>
2026-02-02 12:55:25 +0100

    Ajout d'une route pour les instructions relatives au test en ligne

--- routes/web_applicant.php
+++ routes/web_applicant.php
@@ -9,6 +9,7 @@ Route::group(['prefix' => '{locale}', 'middleware' => 'setLocale'], function () 
 
     Route::get('/scholarship/test', [ApplicantController::class, 'test'])->name('scholarship.test');
     Route::post('/scholarship/test', [ApplicantController::class, 'authenticateApplicants'])->name('scholarship.authenticate');
+    Route::get('/scholarship/test/instructions', [ApplicantController::class, 'instructions'])->name('scholarship.instructions');
 
     Route::get('/scholarship/register', [ApplicantController::class, 'register'])->name('scholarship.register');
     // ->middleware('checkSubmission');
