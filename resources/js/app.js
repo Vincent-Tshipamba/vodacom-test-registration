5636df969437197bff43e6e530c1aca5d38902fe
Vincent-Tshipamba <tshipambalubobo80@gmail.com>
2026-02-02 12:56:52 +0100

    Reorganizing the imports in the js main file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,19 +1,23 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+
+import $ from 'jquery';
+window.$ = window.jQuery = $
 
 import 'sweetalert2';
+import Swal from 'sweetalert2';
+window.Swal = Swal;
+
 import 'flowbite';
 import 'jquery-validation';
 
-window.Alpine = Alpine;
 
 Alpine.start();
 
 import 'preline';
 import * as pdfjsLib from 'pdfjs-dist/build/pdf';
-
-import $ from 'jquery';
 
 import DataTable from 'datatables.net-dt';
 
