1a551510ab0673af47237af37b14f7311e37bdf0
Vincent-Tshipamba <tshipambalubobo80@gmail.com>
2026-02-02 14:59:07 +0100

    Ajout d'un fichier de langue pour le test

---
+++ lang/fr/test.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'evaluation' => 'Évaluation',
+    'start_hour' => 'Heure de début',
+    'end_hour' => 'Heure de fin',
+    'duration' => 'Durée',
+    'start_date' => 'Date de début',
+    'end_date' => 'Date de fin'
+];
