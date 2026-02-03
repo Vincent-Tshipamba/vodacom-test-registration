7c7ad1d99927d0d690d5bded440b8ff9d4b26f0a
Vincent-Tshipamba <tshipambalubobo80@gmail.com>
2026-02-03 16:16:12 +0100

    Ajout de quelques cles francaises pour l'interface de test

--- lang/fr/test.php
+++ lang/fr/test.php
@@ -1,10 +1,44 @@
 <?php
 
 return [
+    'edition' => 'Édition en cours',
+
+    'status_phase' => 'Statut de la phase',
+
     'evaluation' => 'Évaluation',
+
+    'start_time' => 'Date de début',
+    'end_time' => 'Date de fin',
+    'start_date' => 'Date de début',
+    'end_date' => 'Date de fin',
+
     'start_hour' => 'Heure de début',
     'end_hour' => 'Heure de fin',
-    'duration' => 'Durée',
-    'start_date' => 'Date de début',
-    'end_date' => 'Date de fin'
+
+    'duration' => 'Durée (minutes)',
+
+    'total_questions' => 'Nombre total de questions',
+    'passing_score' => 'Score de passage',
+
+    'define' => 'Définir',
+    'modify' => 'Modifier',
+    'save' => 'Enregistrer',
+
+    'total_explaination' => 'Le nombre de tous les candidats retenus pour passer le test.',
+    'female_explaination' => 'Le nombre des filles retenus.',
+    'male_explaination' => 'Le nombre des garçons retenus.',
+    'start_explaination' => 'Le nombre de candidats ayant déjà passé le test.',
+    'female' => 'Filles',
+    'male' => 'Garçons',
+    'stats_candidats' => 'Stats des candidats',
+    'stats_test' => 'Stats de l\'évaluation',
+    'stats_questions' => 'Stats des questions',
+    'stats_questions_explaination' => 'Le nombre total de questions utilisées dans le test.',
+    'details' => 'Détails',
+    'success' => 'Réussites',
+    'failed' => 'Échecs',
+    'success_explaination' => 'Le nombre de candidats ayant réussi le test.',
+    'success_female_explaination' => 'Le nombre de filles ayant réussi le test.',
+    'success_male_explaination' => 'Le nombre de garçons ayant réussi le test.',
+    'failed_explaination' => 'Le nombre de candidats ayant échoué au test.',
 ];
