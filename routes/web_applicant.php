556902780b801c035f6425270b8ba4f90ffadcd8
Vincent-Tshipamba <tshipambalubobo80@gmail.com>
2026-03-20 14:40:53 +0100

    Adding missing routes for managing an exam (starting, saving, submitting, etc.)

--- routes/web_applicant.php
+++ routes/web_applicant.php
@@ -10,6 +10,11 @@ Route::group(['prefix' => '{locale}', 'middleware' => 'setLocale'], function () 
     Route::get('/scholarship/test', [ApplicantController::class, 'test'])->name('scholarship.test');
     Route::post('/scholarship/test', [ApplicantController::class, 'authenticateApplicants'])->name('scholarship.authenticate');
     Route::get('/scholarship/test/instructions', [ApplicantController::class, 'instructions'])->name('scholarship.instructions');
+    Route::get('/scholarship/test/submitted', [ApplicantController::class, 'submitted'])->name('scholarship.exam.submitted');
+    Route::post('/scholarship/test/start', [ApplicantController::class, 'startExam'])->name('scholarship.exam.start');
+    Route::post('/scholarship/test/save', [ApplicantController::class, 'saveExamProgress'])->name('scholarship.exam.save');
+    Route::post('/scholarship/test/violation', [ApplicantController::class, 'registerExamViolation'])->name('scholarship.exam.violation');
+    Route::post('/scholarship/test/submit', [ApplicantController::class, 'submitExam'])->name('scholarship.exam.submit');
 
     Route::get('/scholarship/register', [ApplicantController::class, 'register'])->name('scholarship.register');
     // ->middleware('checkSubmission');
