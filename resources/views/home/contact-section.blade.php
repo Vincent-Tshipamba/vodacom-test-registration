83f7491bc72f18be02b1bec78bdb0ab08ef3bb83
Vincent-Tshipamba <tshipambalubobo80@gmail.com>
2026-04-03 19:28:44 +0100

    Ajout d'une carte pour la presentation de la localisation de la direction generale

--- resources/views/home/contact-section.blade.php
+++ resources/views/home/contact-section.blade.php
@@ -55,6 +55,12 @@
                         {{ __('contact.info.city') }}</p>
                 </div>
             </div>
+            <div class="w-full mx-auto mt-8">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3978.5717519984055!2d15.309259640207772!3d-4.303017900000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1a6a33001419536d%3A0x71f18e09c45f198f!2sVodacom%20DRC%20-%20VodaPark!5e0!3m2!1sfr!2scd!4v1775229829430!5m2!1sfr!2scd"
+                    width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
         </div>
     </div>
 </section>
